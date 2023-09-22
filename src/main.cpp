7302ea5001722004e22aba15560e8108399eb03f
netoe1 <elynetobr@outlook.com>
2023-09-21 23:01:45 -0300

    dskdksds

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <locale>
+#include "../marg/marg.hpp"
+
+
+
+int main(int argc,char** argv){
+    ProgramArgs program_args(argc,argv);
+
+
+    if(program_args.findAnArgument("create") != "undefined" && program_args.argv.at(2) == "create"){
+        if(program_args.findAnArgument("create") != "undefined" && program_args.argv.at(2) == "create"){
+        
+    }
+    }
+}
